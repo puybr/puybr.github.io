109c9ceead7b54a1551bea350276b781f8d47b06
Chloe Urisohn <chloeurisohn@gmail.com>
2021-09-28 18:44:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# 👨‍💻 Personal Portfolio
+
+![My Portfolio Site](./error.png)
+
+### Live Demo [here](https://ghost-goblin.github.io/personal-site/)
+
+## 🛠️ Built with
+* HTML
+* SCSS _[(Preprocessor)](https://sass-lang.com/)_
+* JavaScript
+* [Typed.js](https://www.javascripting.com/view/typed-js)
+- _Bundled with Webpack_
+
+## 💡 Features
+* Mobile Responsive
+* Animated typing effect with `Typed.js`
+* Dynamic scrolling
+
+* * *
+
+* A useful guide on [how to setup SASS in your project](https://dev.to/chrissiemhrk/how-to-setup-sass-in-your-project-2bo1)
+* [How to use Responsive media queries in SCSS](https://learnopidia.com/responsive-media-queries-scss/)
+
+> Run `npm run sass` in the terminal of the `.\dist\scss\` directory
+
+- - -
+
+* [Additional steps to take when deploying a subfolder to Github Pages](https://gist.github.com/cobyism/4730490):
+1. `git add dist && git commit -m "Initial dist subtree commit"`
+2. `git subtree push --prefix dist origin gh-pages`
+
+> Check out: [Getting Hired](https://www.theodinproject.com/paths/full-stack-javascript/courses/getting-hired/lessons/building-your-personal-website): building your personal website.
+
+- - -
+
+Made with **Helvetica** 🖤 | ✏️ Illustrations by: _[Wham!](https://www.ls.graphics/illustrations/wham)_
---
+++ src/index.js
@@ -0,0 +1,201 @@
+import Typed from 'typed.js';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+// Typing effect with typed.js
+const typed = new Typed('.text-animation', {
+    strings: ['coder.', 'graphic designer.', 'computer nerd.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
+
+
+let myProjects = [
+  {
+    title: 'To-do List',
+    demo: 'https://ghost-goblin.github.io/to-do-list/',
+    code: 'https://github.com/ghost-goblin/to-do-list/',
+    description: '✔️ To-Do List created with JavaScript ES6 Modules',
+  },
+  {
+    title: 'Pizza Hex',
+    demo: 'https://ghost-goblin.github.io/pizza-hex/',
+    code: 'https://github.com/ghost-goblin/pizza-hex/',
+    description: '🧙‍♀️ 16-bit Arcade Game made with PhaserJS',
+  },
+  {
+    title: 'My Library',
+    demo: 'https://ghost-goblin.github.io/library/',
+    code: 'https://github.com/ghost-goblin/library/',
+    description: '📕 Library Project using JavaScript Objects',
+  },
+  {
+    title: 'Restaurant Page',
+    demo: 'https://ghost-goblin.github.io/restaurant-page/',
+    code: 'https://github.com/ghost-goblin/restaurant-page/',
+    description: '🍕 Restaurant site with dynamic tabbed browsing functionality',
+  },
+  {
+    title: 'Tic Tac Toe',
+    demo: 'https://ghost-goblin.github.io/tic-tac-toe/',
+    code: 'https://github.com/ghost-goblin/tic-tac-toe/',
+    description: '🕹️ A Tic Tac Toe game you can play in your browser',
+  },
+  {
+    title: 'Calculator',
+    demo: 'https://ghost-goblin.github.io/calculator/',
+    code: 'https://github.com/ghost-goblin/calculator/',
+    description: '🧮 Calculator App with JavaScript',
+  },
+  {
+    title: 'Pixelator',
+    demo: 'https://ghost-goblin.github.io/etch-a-sketch/',
+    code: 'https://github.com/ghost-goblin/etch-a-sketch/',
+    description: '✏️ Etch-A-Sketch made with HTML, CSS & JavaScript',
+  },
+  {
+    title: 'Rock Paper Scissors',
+    demo: 'https://ghost-goblin.github.io/rock-paper-scissors/',
+    code: 'https://github.com/ghost-goblin/rock-paper-scissors/',
+    description: '✋ Play Rock, Paper, Scissors with JavaScript',
+  }
+];//end of my projects
+
+let mySkills = [
+  {
+    type: 'HTML',
+    icon: `<i class='fab fa-html5'></i>`,
+  },
+  {
+    type: 'CSS',
+    icon: `<i class='fab fa-css3-alt'></i>`,
+  },
+  {
+    type: 'JAVASCRIPT',
+    icon: `<i class='fab fa-js-square'></i>`,
+  },
+  {
+    type: 'GIT',
+    icon: `<i class="fab fa-git-alt"></i>`,
+  },
+  {
+    type: 'BASH',
+    icon: `<i class="fas fa-code"></i>`,
+  },
+  {
+    type: 'REST API',
+    icon: `<i class="fas fa-server"></i>`,
+  },
+  {
+    type: 'SQL',
+    icon: `<i class="fas fa-database"></i>`,
+  }
+];//end of my skills
+
+
+//render my projects
+const projectContainer = document.querySelector('.project-container');
+myProjects.forEach((project) => {
+  const p = `
+          <div class="card">
+            <h3>${project.title}<hr></h3>
+            <span>
+              <div class="project-url">
+                <a href=${project.demo} target="_blank"><div class="project-icons"><i class="fas fa-laptop-code"></i></div>Live Demo</a>
+              </div>
+              <div class="project-url">
+                <a href=${project.code} target="_blank"><div class="project-icons"><i class="fas fa-file-code"></i></div>Code</a>
+              </div>
+              <p>${project.description}</p>
+            </span>
+          </div>
+          `;
+          projectContainer.insertAdjacentHTML('beforeend', p);
+});
+
+
+//render skills
+const skillsContainer = document.querySelector('.skills-container');
+mySkills.forEach((skill) => {
+  const s = `
+                <div class="skill">
+                <div class="skill-icons">${skill.icon}</div>
+                <p><b>${skill.type}</b></p>
+                </div>
+            `;
+  skillsContainer.insertAdjacentHTML('beforeend', s); 
+});
+
+
+// 🍔 Hamburger Icon
+const hamburgerIcon = document.querySelector('#hamburger');
+hamburgerIcon.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('nav').style.display = 'flex';
+  document.querySelectorAll('.sections').forEach((section) => {
+    if (section.style.display === 'block') {
+      section.style.display = 'none';
+      document.querySelector('nav').style.display = 'none';
+    } else {
+      section.style.display = 'block';
+    }
+  });
+
+});
+
+
+
+/* Scrolling Functionality */
+const homeButton = document.querySelector('#home');
+const homeSection = document.querySelector('.home');
+homeButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  homeSection.scrollIntoView({behavior: "smooth"});
+});
+
+const aboutButton = document.querySelector('#about');
+const aboutShortcut = document.querySelector('#aboutShortcut');
+const aboutSection = document.querySelector('.about');
+aboutButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutSection.scrollIntoView({behavior: "smooth"});
+});
+aboutShortcut.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutSection.scrollIntoView({behavior: "smooth"});
+});
+
+const skillsButton = document.querySelector('#skills');
+const skillsSection = document.querySelector('.skills');
+skillsButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  skillsSection.scrollIntoView({behavior: "smooth"});
+});
+
+const projectsButton = document.querySelector('#projects');
+const projectShortcut = document.querySelector('#projectShortcut');
+const projectsSection = document.querySelector('.projects');
+projectsButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  projectsSection.scrollIntoView({behavior: "smooth"});
+});
+projectShortcut.addEventListener('click', (e) => {
+  e.preventDefault();
+  projectsSection.scrollIntoView({behavior: "smooth"});
+});
+
+// window.addEventListener('scroll', function() {
+//   console.log('scrolling');
+//   console.log(window.pageYOffset);
+// });
+
+
+// F O O T E R
+document.querySelector('footer').innerHTML = `
+                                            <hr>
+                                            © ${new Date().getFullYear()}
+                                            <i class="fas fa-arrow-circle-up"></i>
+                                            `;
